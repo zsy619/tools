31dc8afb0938a5c758f2eacd1279951ddc44c2ac
朱书彦 <zsy619@163.com>
2023-09-16 17:56:01 +0800

    【feat】VerifyPassword强密码校验方法

--- xstring/check_pwd.go
+++ xstring/check_pwd.go
@@ -50,3 +50,33 @@ func PasswordCheck(minLength, maxLength, minLevel int, pwd string) error {
 	}
 	return nil
 }
+
+// VerifyPassword 密码校验规则: 必须包含数字、大写字母、小写字母、特殊字符(如.@$!%*#_~?&^)至少3种的组合且长度在8-16之间
+func VerifyPassword(minLength, maxLength int, pwd string) bool {
+	if len(pwd) < minLength || len(pwd) > maxLength {
+		return false
+	}
+	// 过滤掉这四类字符以外的密码串,直接判断不合法
+	re, err := regexp.Compile(`^[a-zA-Z0-9.@$!%*#_~?&^]{8,16}$`)
+	if err != nil {
+		return false
+	}
+	match := re.MatchString(pwd)
+	if !match {
+		return false
+	}
+
+	level := 0
+	patternList := []string{`[0-9]+`, `[a-z]+`, `[A-Z]+`, `[.@$!%*#_~?&^]+`}
+	for _, pattern := range patternList {
+		match, _ := regexp.MatchString(pattern, pwd)
+		if match {
+			level++
+		}
+	}
+	if level < 3 {
+		return false
+	}
+
+	return true
+}
--- xstring/check_pwd_test.go
+++ xstring/check_pwd_test.go
@@ -1,6 +1,8 @@
 package xstring
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPasswordCheck(t *testing.T) {
 	type args struct {
@@ -20,6 +22,36 @@ func TestPasswordCheck(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := PasswordCheck(tt.args.minLength, tt.args.maxLength, tt.args.minLevel, tt.args.pwd); (err != nil) != tt.wantErr {
 				t.Errorf("PasswordCheck() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestVerifyPassword(t *testing.T) {
+	type args struct {
+		minLength int
+		maxLength int
+		pwd       string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test1", args{minLength: 8, maxLength: 32, pwd: "fb0da#07E"}, true,
+		},
+		{
+			"test2", args{minLength: 8, maxLength: 32, pwd: "12358678"}, false,
+		},
+		{
+			"test2", args{minLength: 8, maxLength: 32, pwd: "fb0da#07"}, true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := VerifyPassword(tt.args.minLength, tt.args.maxLength, tt.args.pwd); got != tt.want {
+				t.Errorf("VerifyPassword() = %v, want %v", got, tt.want)
 			}
 		})
 	}
