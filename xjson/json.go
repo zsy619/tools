cb8f9659d826e60535f76e6090f44673f238ef1c
朱书彦 <zsy619@163.com>
2025-01-06 18:32:08 +0800

    【fix】修订json

--- xjson/json.go
+++ xjson/json.go
@@ -6,6 +6,7 @@ import (
 )
 
 // https://mp.weixin.qq.com/s?__biz=MzkyMzIyNjIxMQ==&mid=2247485083&idx=1&sn=e828d5b3d30727bb13a16ca00d63324d&chksm=c1e91f97f69e9681ef2a6b79674c7202869bebc478713caf3fc8e34c4021e6ee6da37a95949f&scene=21#wechat_redirect
+// https://www.jianshu.com/p/040f8b735a47
 var JSON = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func init() {
@@ -13,9 +14,11 @@ func init() {
 }
 
 func Unmarshal(data []byte, v interface{}) error {
-	return JSON.Unmarshal(data, v)
+	jsonNew := jsoniter.ConfigCompatibleWithStandardLibrary
+	return jsonNew.Unmarshal(data, v)
 }
 
 func Marshal(v interface{}) ([]byte, error) {
-	return JSON.Marshal(v)
+	jsonNew := jsoniter.ConfigCompatibleWithStandardLibrary
+	return jsonNew.Marshal(v)
 }
